75c0baf172102f431cda69c41fcabb19b7a281a9
Vinicius <viniciusep181@gmail.com>
2024-07-26 16:39:57 -0300

    fix: auto transform

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    transformOptions: { enableImplicitConversion: true }
+  }))
   app.enableCors()
   await app.listen(process.env.PORT || 3001);
 }
